2bbe2030134640922c70245748c5fe890114b664
ashirul <mdashirulhaquesumon@gmail.com>
2024-02-24 03:02:32 +0600

    update design

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+
 document.addEventListener('DOMContentLoaded', () => {
 
     // Get all "navbar-burger" elements
@@ -20,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
   });
+
   function openTab(evt, tabName) {
     var i, x, tablinks;
     x = document.getElementsByClassName("content-tab");
@@ -33,3 +35,87 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " is-active";
   }
+  
+  var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      40: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      40: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      400: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const accordions = document.querySelectorAll('.accordion-item');
+
+    accordions.forEach(function (accordion) {
+        const header = accordion.querySelector('.accordion-header');
+        const toggle = header.querySelector('.accordion-toggle');
+        const content = accordion.querySelector('.accordion-content');
+
+        header.addEventListener('click', function () {
+            const isOpen = accordion.classList.contains('is-active');
+
+            // Close all accordions
+            accordions.forEach(function (item) {
+                item.classList.remove('is-active');
+                item.querySelector('.accordion-content').style.maxHeight = null;
+            });
+
+            // Open the clicked accordion if it's not already open
+            if (!isOpen) {
+                accordion.classList.add('is-active');
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    });
+});
+
+  
